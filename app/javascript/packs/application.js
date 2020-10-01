033710bb357c5605285f41042b925f5108ca5c38
Akito Tsutsui <akito.tsutsui@gmail.com>
2020-10-01 23:36:15 +0900

    feat: subscription用のjsを作成

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,17 +14,18 @@ require('dotenv').config()
 // const imagePath = (name) => images(name, true)
 
 // for stripe
-const form = document.querySelector("#payment-form")
+const cardElement = document.querySelector("#payment-form")
 
-if (form){
+if (cardElement !== null) { setupStripe()}
+function setupStripe(){
     const public_key = process.env.STRIPE_PUBLIC_KEY
     const stripe = Stripe(public_key)
     const elements = stripe.elements()
     const card = elements.create('card', { hidePostalCode: true})
     card.mount('#card-element')
+    const displayError = document.getElementById("card-errors")
 
     card.addEventListener("change", (event) => {
-        const displayError = document.getElementById("card-errors")
         if (event.error){
             displayError.textContent = event.error.message
         } else {
@@ -32,29 +33,38 @@ if (form){
         }
     })
 
+    const form = document.querySelector("#subscription-payment-form")
+
     form.addEventListener("submit", (event) => {
         event.preventDefault()
-        let data = {
+        let name = form.querySelector("#name_on_card").value
+
+        let payment_data = {
             payment_method: {
                 card: card,
                 billing_details: {
-                    name: form.querySelector("#name_on_card").value
+                    name: name
                 }
             }
         }
 
-        // TODO 後で消す
-        console.log(data)
-
-        stripe.confirmCardPayment(form.dataset.paymentIntentId, data).then((result) => {
+        payment_data.payment_method_data.type = "card"
+        stripe.confirmCardPayment(payment_data.payment_method_data).then((result) => {
             if (result.error) {
-                const errorElement = document.getElementById("card-errors")
-                errorElement.textContent = result.error.message
+                displayError.textContent = result.error.message
             } else {
+                addHiddenField(form, "payment_method_id", result.paymentMethod.id)
                 form.submit()
             }
         })
 
     })
+}
 
+function addHiddenField(form, name, value) {
+    let hiddenInput = document.createElement("input")
+    hiddenInput.setAttribute("type", "hidden")
+    hiddenInput.setAttribute("name", name)
+    hiddenInput.setAttribute("value", value)
+    form.appendChild(hiddenInput)
 }
